35920c0db46e38077e69a6f8156fc7335ed2e15c
repsukka <81526112+repsukka@users.noreply.github.com>
2021-11-09 22:22:12 +0200

    pikkumuutos

--- login.php
+++ login.php
@@ -3,10 +3,6 @@
 require('headers.php');
 require('functions.php');
 
-$db = getDbConnection();
-createTable($db);
-exit;
-
 //Tarkistetaan tuleeko palvelimelle basic login tiedot (Authorization: Basic asfkjsafdjsajflkasj)
 if( isset($_SERVER['PHP_AUTH_USER']) ){
     if(checkUser(getDbConnection(), $_SERVER['PHP_AUTH_USER'],$_SERVER["PHP_AUTH_PW"] )){
